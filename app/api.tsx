f36004c5e0d12ecd45289d7811f5350f99b82f70
Jrmyyyyy <164606512+Jrmyyyyy@users.noreply.github.com>
2024-05-26 23:57:27 +0800

    Initial commit

---
+++ app/api.tsx
@@ -0,0 +1,35 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useState, useEffect, SetStateAction } from "react";
+import { Inter } from "next/font/google";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+
+const runModel = async (userInput: string, History: { role: string; parts: { text: string; }[]; }[], setIsGenerating: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }, setUserInput: { (value: SetStateAction<string>): void; (arg0: string): void; }) => {
+  setIsGenerating(true);
+  try {
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+    const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-pro-latest",
+      systemInstruction: "You are a naturopathic doctor",
+    });
+
+    const chat = model.startChat({
+      history: History,
+    });
+
+    const result = await chat.sendMessage(userInput);
+    const response = await result.response;
+    const text = await response.text();
+
+    // Handle the response text here
+    // For example, you could update the History state or handle the response data in some other way
+  } catch (error) {
+    console.error("There's an error while running your model:", error);
+  } finally {
+    setIsGenerating(false);
+    setUserInput("");
+  }
+};
+
+export default runModel;
