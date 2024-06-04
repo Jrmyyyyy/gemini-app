bdfdac042d3e3771e8f950c5cc32fbc85600fda4
Jasmin Rodrigo <secuyajasmine@gmail.com>
2024-06-04 11:45:40 +0800

    remove clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-//clerk
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,18 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
